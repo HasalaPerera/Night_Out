b35392ede20d1ffc6243c37896dd9c252eef60ba
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2024-04-10 13:57:26 +0530

    Update info.dart
    Infor dart

--- night_out_app/lib/info.dart
+++ night_out_app/lib/info.dart
@@ -36,7 +36,28 @@ class _AboutEmergencyPageState extends State<AboutEmergencyPage> {
                     fontWeight: FontWeight.bold,
                     color: Colors.white), // Set text color to white
               ),
-             
+              SizedBox(height: 20.0),
+              Text(
+                'Night Out is a mobile app that is dedicated to providing you with necessary information regarding the nightlife of Sri Lanka such as events, concerts, shows, restaurant offers, etc. Our main objective is to provide you with reliable and accurate information so you can have a good time out there without having to worry. Night Out can be considered as the first proper digital footprint in mobile apps that gives you up-to-date information about the nightlife. And also we are glad to help out business entities that are specialized in nighttime activities.',
+                textAlign: TextAlign.justify,
+                style:
+                    TextStyle(color: Colors.white), // Set text color to white
+              ),
+              SizedBox(height: 40.0),
+              Text(
+                'Emergency Contacts',
+                style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white), // Set text color to white
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Police: 118 / 119\nFire Department: 110\nAmbulance: 1990\nGovernment Information Center: 1919',
+                textAlign: TextAlign.center,
+                style:
+                    TextStyle(color: Colors.white), // Set text color to white
+              ),
             ],
           ),
         ),
